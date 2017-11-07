c882a6f9952b542bd6dc92b41695586d497aa658
kelvin kharhys <nrhyska@gmail.com>
2017-11-07 21:38:11 +0300

    moved template to separate repo

--- config/backpack.config.js
+++ config/backpack.config.js
@@ -5,7 +5,24 @@ const babelPreset = require('babel-preset-backpack')
 const nodeExternals = require('webpack-node-externals')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
-const config = require('./paths')
+const path = require('path')
+const rootPath = path.resolve(process.cwd())
+const buildPath = path.join(rootPath, 'build')
+const publicBuildPath = path.join(buildPath, 'public')
+
+console.log('rootPath', rootPath)
+
+const config = {
+  rootPath,
+  buildPath,
+  publicBuildPath,
+  publicSrcPath: path.join(rootPath, 'public'),
+  serverSrcPath: path.join(rootPath, 'src'),
+  serverBuildPath: buildPath,
+  userNodeModulesPath: path.join(rootPath, 'node_modules'),
+  publicPath: '/',
+  serverUrl: 'http://localhost:3000'
+}
 
 // This is the Webpack configuration.
 // It is focused on developer experience and fast rebuilds.
